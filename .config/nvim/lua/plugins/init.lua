7a06c3d0b84b053ab97e46bee74e73bc726edc8a
Emmanuel Martínez <emartinez@seebak.com.mx>
2022-09-07 13:07:27 -0500

    Add Bootstrap packer function to neovim configuration and add tmux config file

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -1,3 +1,19 @@
+-- Bootstrapping
+local ensure_packer = function()
+  local fn = vim.fn
+  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+  if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+    vim.cmd [[packadd packer.nvim]]
+    return true
+  end
+  return false
+end
+
+local packer_bootstrap = ensure_packer()
+
+
+-- Packer plugins configuration
 return require('packer').startup(function(use)
   -- Packer can manage itself
   use 'wbthomason/packer.nvim'
@@ -93,5 +109,11 @@ return require('packer').startup(function(use)
       {'mfussenegger/nvim-dap'},
     }
   }
+
+  -- Automatically set up your configuration after cloning packer.nvim
+  -- Put this at the end after all plugins
+  if packer_bootstrap then
+    require('packer').sync()
+  end
 
 end)
--- .zprofile
+++ .zprofile
@@ -1,4 +1,5 @@
 export PYENV_ROOT=$HOME/.pyenv
+export WORKSPACE=/workspace
 export PATH=$PYENV_ROOT/bin:$HOME/.local/bin:$HOME/bin:/usr/local/bin:/usr/sbin
 export PATH=$PATH:/usr/bin:/sbin:/bin:$HOME/.poetry/bin:/workspace/scripts
 export PATH=$PATH:$HOME/.pyenv/versions/sam-cli/bin:.
