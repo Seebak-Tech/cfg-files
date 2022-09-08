d9fdb21c57d6d34421d0d651fc3246a769cac3a2
Luis Díaz <luigirdf@gmail.com>
2022-09-08 01:19:21 -0500

    Adding dependency nvim-surround.

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -88,11 +88,15 @@ return require('packer').startup(function(use)
   use({
     "kylechui/nvim-surround",
     tag = "*", -- Use for stability; omit to use `main` branch for the latest features
-      config = function()
+    config = function()
         require("nvim-surround").setup({
             -- Configuration here, or leave empty to use defaults
         })
-    end
+    end,
+    requires = {
+      "nvim-treesitter/nvim-treesitter",
+      "nvim-treesitter/nvim-treesitter-textobjects",
+    },
   })
 
   -- Projectionist
